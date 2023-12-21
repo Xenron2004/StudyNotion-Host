974a53d10ed47996b0d5f249efbf4388b3ef8b2a
Xenron2004 <126370309+Xenron2004@users.noreply.github.com>
2023-12-21 14:19:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "https://studynotionfinal.onrender.com",
         credentials: true,
     })
 )
